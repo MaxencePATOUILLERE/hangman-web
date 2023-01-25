e2d86ff423636c710ab5e01244fc88a80b1448a6
bremi <remi.buzenac@ynov.com>
2023-01-25 19:23:36 +0100

    resolve cookies issue

--- session.go
+++ session.go
@@ -10,7 +10,10 @@ import (
 
 func onConnect(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "cookie-name")
-	//haveAlreadyCookie(session, w)
+	if session.Values["uid"] != nil {
+		haveAlreadyCookie(session)
+		return
+	}
 	auth, ok := session.Values["authenticated"].(bool)
 	if !ok || !auth && len(playerList) <= 3 {
 		setDatas(session, r, w)
@@ -36,10 +39,19 @@ func setDatas(session *sessions.Session, r *http.Request, w http.ResponseWriter)
 	playerList = append(playerList, usernames[len(playerList)])
 }
 
-func haveAlreadyCookie(session *sessions.Session, w http.ResponseWriter) bool {
-	if session.Values["uid"] != nil {
-		fmt.Fprintf(w, "You have already a session delete cookies.")
-		return true
+func haveAlreadyCookie(session *sessions.Session) {
+	uid := session.Values["uid"].(int)
+	for key, _ := range dictPlayer {
+		if uid == key {
+			return
+		}
 	}
-	return false
+	multiplayerData.Users = append(multiplayerData.Users, uid)
+	if len(playerList) == 0 {
+		dictPlayer[uid] = UserData{userName: usernames[len(playerList)], admin: true}
+	} else {
+		dictPlayer[uid] = UserData{userName: usernames[len(playerList)], admin: false}
+	}
+	multiplayerData.Turn = uid
+	playerList = append(playerList, usernames[len(playerList)])
 }
